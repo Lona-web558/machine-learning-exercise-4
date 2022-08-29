09bf694849817abac017b9e2b38c00f8e021e25f
Lona Matshingana <71089848+Lona-web558@users.noreply.github.com>
2022-08-29 11:02:09 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,6 @@
+print("NumPy array:\n", eye)
+sparse_matrimport numpy as np
+from scipy import sparse
+eye = np.eye(4)
+ix = sparse.csr_matrix(eye)
+print("\nSciPy sparse CSR matrix:\n", sparse_matrix)
